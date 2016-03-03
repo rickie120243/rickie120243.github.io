d3ceda95d79e351d979b274b10444421712dbbcc
rickie120243 <rickie120243@gmail.com>
2016-03-03 17:31:08 +0800

    fix inline js

--- googleLogin.js
+++ googleLogin.js
@@ -2,17 +2,19 @@ function onSignIn(googleUser) {
   var arr = [];
   var profile = googleUser.getBasicProfile();
   var id_token = googleUser.getAuthResponse().id_token;
-  arr.push("ID: " + profile.getId());
-  arr.push("Name: " + profile.getName());
-  arr.push("Image URL: " + profile.getImageUrl());
-  arr.push("Email: " + profile.getEmail());
-  arr.push("ID Token: " + id_token);
-  document.getElementById("loginInfo").innerHTML = arr.join("<br>");  
+  arr.push('ID: ' + profile.getId());
+  arr.push('Name: ' + profile.getName());
+  arr.push('Image URL: ' + profile.getImageUrl());
+  arr.push('Email: ' + profile.getEmail());
+  arr.push('ID Token: ' + id_token);
+  document.getElementById('loginInfo').innerHTML = arr.join('<br>');  
 }
 
-function signOut() {
+function doSignOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
-    document.getElementById("loginInfo").innerHTML = 'User signed out.';
+    document.getElementById('loginInfo').innerHTML = 'User signed out.';
   });
 }
+
+document.getElementById('signOut').addEventListener('click', doSignOut());
