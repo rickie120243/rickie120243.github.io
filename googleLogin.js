4f065dcb589499512ff16d35b8c5fcb4fc9489b7
rickie120243 <rickie120243@gmail.com>
2016-03-03 17:23:52 +0800

    remove console

--- googleLogin.js
+++ googleLogin.js
@@ -6,18 +6,13 @@ function onSignIn(googleUser) {
   arr.push("Name: " + profile.getName());
   arr.push("Image URL: " + profile.getImageUrl());
   arr.push("Email: " + profile.getEmail());
-  document.getElementById("loginInfo").innerHTML = arr.join("<br>");
-  console.log("ID: " + profile.getId());
-  console.log("Name: " + profile.getName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
-  console.log("ID Token: " + id_token);  
+  arr.push("ID Token: " + id_token);
+  document.getElementById("loginInfo").innerHTML = arr.join("<br>");  
 }
 
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     document.getElementById("loginInfo").innerHTML = 'User signed out.';
-    console.log('User signed out.');
   });
 }
