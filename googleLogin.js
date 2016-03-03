a78269f859730baa4635fa1913aa99679215485e
rickie120243 <rickie120243@gmail.com>
2016-03-03 17:09:22 +0800

    show login info

--- googleLogin.js
+++ googleLogin.js
@@ -1,6 +1,12 @@
 function onSignIn(googleUser) {
+  var arr = [];
   var profile = googleUser.getBasicProfile();
   var id_token = googleUser.getAuthResponse().id_token;
+  arr.push("ID: " + profile.getId());
+  arr.push("Name: " + profile.getName());
+  arr.push("Image URL: " + profile.getImageUrl());
+  arr.push("Email: " + profile.getEmail());
+  document.getElementById("loginInfo").innerHTML = arr.join("<br>");
   console.log("ID: " + profile.getId());
   console.log("Name: " + profile.getName());
   console.log("Image URL: " + profile.getImageUrl());
@@ -11,6 +17,7 @@ function onSignIn(googleUser) {
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
+    document.getElementById("loginInfo").innerHTML = 'User signed out.';
     console.log('User signed out.');
   });
 }
